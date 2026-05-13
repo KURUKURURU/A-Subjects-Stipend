0a41da53bf1fa31964ef289a9aa7d1e989f25ba7
KURUKURURU <93101194+KURUKURURU@users.noreply.github.com>
2026-05-12 22:28:39 -0400

    hi

--- a-subject's-stipend/scene/story/introduction.gd
+++ a-subject's-stipend/scene/story/introduction.gd
@@ -13,14 +13,44 @@ extends Node2D
 signal advance
 
 func _ready() -> void:
-	await Speak("Crown Princess Marigold, 
-savior of the kingdom and 
-holder of the silver sword.")
 	await Speak("AMBERWOOD, 
 kingdom of honey.
 
 –A kingdom revered for its beauty.")
 
+	await Speak("a testiment to this beauty 
+would be the kingdom's castle,
+dubbed the \"FIREGARDEN\".")
+	
+	await Speak("Here lies the charming and intelligent [wave]CROWN PRINCESS[/wave], 
+
+savior of the kingdom and 
+holder of the silver sword.")
+
+	await Speak("You are loyal to her, 
+as you are one of her subjects.")
+
+	await Speak("Though it also helps that
+you were her close friend in childhood, 
+
+despite you being a poor lowly noble.")
+
+	await Speak("It truly shows how amazing
+and humble our crown princess is.
+
+You would know though, [wave]childhood friend.")
+
+	await Speak("~~~")
+	await Speak("Enjoy this upcoming journey,
+[pulse freq=0.90 color=#A85511]and whatever else awaits you.[/pulse]
+
+your PRINCESS has called for you.")
+	
+	animation.play("end")
+	await animation.animation_finished
+	
+	get_tree().change_scene_to_file("res://scene/Start.tscn")
+	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		emit_signal("advance")
@@ -36,7 +66,7 @@ func Speak(m: String):
 	message_animation.play("text")
 	await message_animation.animation_finished
 	message.text = m
-	message.bbcode_enabled = false
+	#message.bbcode_enabled = false
 	
 	await advance
 	message.text = ""
