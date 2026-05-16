4baa98bcc37d52f5ae9c1aa6acd769c3d4664f26
KURUKURURU <93101194+KURUKURURU@users.noreply.github.com>
2026-05-15 21:41:26 -0400

    working like a dog day and night

---
+++ a-subject's-stipend/scene/story/s_salary.gd
@@ -0,0 +1,25 @@
+extends TextureProgressBar
+@onready var amount: RichTextLabel = $amount
+@onready var minus: TextureButton = $minus
+@onready var plus: TextureButton = $plus
+@onready var label: RichTextLabel = $label
+
+@onready var _ScrollContainer = $"../.."
+
+func _ready() -> void:
+	self.value = 0
+	plus.pressed.connect(adding)
+	minus.pressed.connect(subtracting)
+
+
+func _process(delta: float) -> void:
+	amount.text = str(self.value)
+
+func adding():
+	var frank = self
+	_ScrollContainer.adding(frank)
+	
+func subtracting():
+	var frank = self
+	_ScrollContainer.subtracting(frank)
+	
--- a-subject's-stipend/scene/story/scroll_container.gd
+++ a-subject's-stipend/scene/story/scroll_container.gd
@@ -5,28 +5,39 @@ extends ScrollContainer
 @onready var emergency: TextureProgressBar = $thing/Emergency
 @onready var emergency_label: RichTextLabel = $thing/Emergency/label
 @onready var emergency_amount: RichTextLabel = $thing/Emergency/amount
+@onready var bad: AudioStreamPlayer = $bad
 
 @onready var coin: AudioStreamPlayer = $coin
+#@onready var _ScrollContainer: AudioStreamPlayer = $"../.."
 
 #@onready var servants_salary_amount: RichTextLabel = $thing/Emergency/amount
 
 
 func _ready() -> void:
-	emergency.value = 0
 	total_money.value = 3000
 
 
 func _process(delta: float) -> void:
-	emergency_amount.text = str(emergency.value)
 	amount.text = str(total_money.value) + " gold left!"
 
 
-func subtracting() -> void:
+func subtracting(path) -> void:
 	coin.play()
-	emergency.value = emergency.value - 100
+	
+	var frank = path
+	
+	frank.value = frank.value - 100
 	total_money.value = total_money.value + 100
-func adding() -> void:
+	
+func adding(path) -> void:
+	if total_money.value == 0:
+		bad.play()
+		return
+
 	coin.play()
+	
+	var frank = path
+	
 	if total_money.value != 0:
-		emergency.value = emergency.value + 100
+		frank.value = frank.value + 100
 		total_money.value = total_money.value - 100
